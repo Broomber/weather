f9100af0ea23060380d9d84c63a738b9d949c054
Bondarenko Yuriy <bondarenko.yuriy.1992@gmail.com>
2019-12-15 12:15:41 +0600

    Updates

---
+++ precache-manifest.eccd8d332db86ac5e89c5a05c86532e7.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f89bb1334883d04e0af5aabafa8b11f2",
+    "url": "/Broomber/weather/index.html"
+  },
+  {
+    "revision": "e4f8030ad8514d887a38",
+    "url": "/Broomber/weather/static/css/main.bce82d78.chunk.css"
+  },
+  {
+    "revision": "914749038255aa1fdfd7",
+    "url": "/Broomber/weather/static/js/2.af56837e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Broomber/weather/static/js/2.af56837e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e4f8030ad8514d887a38",
+    "url": "/Broomber/weather/static/js/main.415039a6.chunk.js"
+  },
+  {
+    "revision": "85e9437870582aeeeba8",
+    "url": "/Broomber/weather/static/js/runtime-main.d96d6158.js"
+  },
+  {
+    "revision": "01dcf602cb7f7116bde159f25c4bbe89",
+    "url": "/Broomber/weather/static/media/Lato-Bold.01dcf602.ttf"
+  },
+  {
+    "revision": "2fe27d9d10cdfccb1baef28a45d5ba90",
+    "url": "/Broomber/weather/static/media/Lato-Light.2fe27d9d.ttf"
+  },
+  {
+    "revision": "fa62c04b03d3f7d2333a33c311d9747d",
+    "url": "/Broomber/weather/static/media/Lato-Regular.fa62c04b.ttf"
+  },
+  {
+    "revision": "8c4c2f61c8df244db674b432c55c9478",
+    "url": "/Broomber/weather/static/media/cloud.8c4c2f61.svg"
+  },
+  {
+    "revision": "eb1241e2f7993961aa7a4cbd552707b9",
+    "url": "/Broomber/weather/static/media/location.eb1241e2.svg"
+  },
+  {
+    "revision": "877bfaf3d726e72494e3f5c192da14be",
+    "url": "/Broomber/weather/static/media/partly_cloudy.877bfaf3.svg"
+  },
+  {
+    "revision": "64cfe69f778b37e09e4b0eb803f70c56",
+    "url": "/Broomber/weather/static/media/rain.64cfe69f.svg"
+  },
+  {
+    "revision": "45dbd80ccf32a9b0b5cd15c60a6e11a2",
+    "url": "/Broomber/weather/static/media/storm.45dbd80c.svg"
+  },
+  {
+    "revision": "8ce7777bc82e41386fc8a37dce965fdc",
+    "url": "/Broomber/weather/static/media/sun.8ce7777b.svg"
+  }
+]);
